5042445fee84ccd0f929da6634b2ce0f7fb2eeb7
LashaBidzina <80463840+LashaBidzina@users.noreply.github.com>
2022-05-01 16:52:57 +0400

    auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,13 +2,33 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const otpGenerator = require('otp-generator');
-
-router.get('/users', async (req, res) => {
-    const users = await User.find();
-    res.json({ users: users });
-});
-
 const { body, validationResult } = require('express-validator');
+const sendMail = require('../libs/mail');
+const jwt = require('jsonwebtoken');
+
+const ensureToken = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+};
+
+router.get('/users', ensureToken, async (req, res) => {
+    jwt.verify(req.token, process.env.TOKEN_SECRET, (err, data) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            User.find().then((users) => {
+                res.json({ users });
+            });
+        }
+    });
+});
 
 router.post('/users', body('email').isEmail(), async (req, res) => {
     const errors = validationResult(req);
@@ -36,8 +56,37 @@ router.post('/users', body('email').isEmail(), async (req, res) => {
         });
 
         await user.save();
-        res.json({ user: user });
+        res.send('Check your email for OTP');
+
+        const mailOptions = {
+            from: 'letterboxd.v2@gmail.com', // sender address
+            to: [email], // list of receivers
+            subject: 'Your one time password', // Subject line
+            html: `<h1>${otp}</h1>`, // plain text body
+        };
+
+        sendMail(mailOptions);
     }
+});
+
+router.post('/users/verifyOTP', (req, res) => {
+    const { email, otp } = req.body;
+
+    User.findOne({ email: email })
+        .then((user) => {
+            if (user.otp === otp) {
+                const token = jwt.sign(
+                    { _id: user._id },
+                    process.env.TOKEN_SECRET
+                );
+                res.json({ message: 'OTP verified', token });
+            } else {
+                res.json({ message: 'OTP not verified' });
+            }
+        })
+        .catch((err) => {
+            res.json({ message: err });
+        });
 });
 
 module.exports = router;
