6430687e0425bfaf25f078d9df1174bc8d62fa80
Sickly-bip <nikafarsa12@gmail.com>
2022-05-01 17:29:58 +0400

    Update UI Component

--- client/components/UI/Input.js
+++ client/components/UI/Input.js
@@ -1,12 +1,13 @@
 import Styles from '../../styles/Input.module.css';
+import {forwardRef} from "react";
 
-const Input = ({label, type, name, placeholder, autocomplete}) => {
+const Input = forwardRef(function Input({label, type, name, placeholder, autocomplete}, ref) {
     return (
         <div className={Styles['input-group']}>
             <label>{label}</label>
-            <input type={type ?? 'text'} name={name} placeholder={placeholder} autoComplete={autocomplete}/>
+            <input ref={ref} type={type ?? 'text'} name={name} placeholder={placeholder} autoComplete={autocomplete.toString()}/>
         </div>
     )
-}
+})
 
 export default Input
--- client/pages/auth.js
+++ client/pages/auth.js
@@ -3,16 +3,31 @@ import CardHeader from "../components/UI/CardHeader";
 import CardBody from "../components/UI/CardBody";
 import Input from "../components/UI/Input";
 import Button from "../components/UI/Button";
+import {useRef} from "react";
+import axios from "axios";
 
 const Auth = () => {
+    const emailRef = useRef();
+
+    const sendOTPHandler = () => {
+        const email = emailRef.current.value;
+        axios.post('http://localhost:3003/users', {email, username: 'Sickly', avatar: '1'})
+            .then(response => {
+                alert('OTP sent, check email');
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    };
+    
     return (
         <div className={"my-auto"}>
             <Card>
                 <CardHeader title={"Log in"}/>
                 <CardBody>
                     <div className={"relative"}>
-                        <Input label={"E-mail"} name={"email"} placeholder={"example@mail.com"} autocomplete={false}/>
-                        <button className={"text-[#E9A6A6] hover:bg-[#E9A6A6] hover:text-[#1F1D36] transition ease-in-out delay-100 h-10 mb-4 px-5 absolute bottom-[-15px] right-0 rounded-lg text-sm"}>Send OTP</button>
+                        <Input ref={emailRef} label={"E-mail"} name={"email"} placeholder={"example@mail.com"} autocomplete={false}/>
+                        <button onClick={sendOTPHandler} className={"text-[#E9A6A6] hover:bg-[#E9A6A6] hover:text-[#1F1D36] transition ease-in-out delay-100 h-10 mb-4 px-5 absolute bottom-[-15px] right-0.5 rounded-lg text-sm"}>Send OTP</button>
                     </div>
                     <Input label={"OTP"} name={"otp"} placeholder={"Enter OTP"} autocomplete={false}/>
                     <Button className={"w-[100%] py-3"}>Authenticate</Button>
