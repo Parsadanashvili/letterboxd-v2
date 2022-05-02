c3d48d713f9059c8897c7de892e164921a4d5b1e
LashaBidzina <80463840+LashaBidzina@users.noreply.github.com>
2022-05-02 04:32:33 +0400

    otp delete

--- server/models/Token.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const schema = mongoose.Schema;
-
-const tokenSchema = new schema({
-    userId: { type: String, required: true },
-    token: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Token', tokenSchema);
