f010ebb3f2c9490fdd5911d06c3b4bd438594a59
Sickly-bip <nikafarsa12@gmail.com>
2022-05-02 04:36:07 +0400

    Header Fix

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,29 +2,36 @@ import Button from "./UI/Button";
 import Styles from "../styles/Header.module.css"
 import Link from "next/link";
 import {useSelector} from "react-redux";
+import {useEffect, useState} from "react";
 
 const Header = () => {
-    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    let stateIsLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
+    useEffect(() => {
+        setIsLoggedIn(stateIsLoggedIn);
+    }, [stateIsLoggedIn]);
 
     return (
         <div className={Styles.header}>
             <div className={Styles.header__inner}>
                 <div className={"h-[100%]"}>
                     <Link href={"/"}>
-                        <img className={"w-[128px]"} src={"/logo.svg"} alt={"Logo"}/>
+                        <img className={"w-[128px] cursor-pointer"} src={"/logo.svg"} alt={"Logo"}/>
                     </Link>
                 </div>
 
                 <div className={""}>
-                    {isLoggedIn ? (
-                        <Link href={"/profile"} passHref>
+                    {isLoggedIn ?
+                        <Link href={"/profile"}>
                             <Button className={"py-1.5 px-14"}>Profile</Button>
                         </Link>
-                    ) : (
-                        <Link href={"/auth"} passHref>
+                        :
+                        <Link href={"/auth"}>
                             <Button className={"py-1.5 px-14"}>Login</Button>
                         </Link>
-                    )}
+                    }
                 </div>
             </div>
         </div>
