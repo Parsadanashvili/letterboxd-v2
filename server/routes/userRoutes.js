1d9d423e421c5abb8a599a829323def2fd2ae93e
LashaBidzina <80463840+LashaBidzina@users.noreply.github.com>
2022-05-04 21:25:05 +0400

    avatar

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,7 @@ const User = require('../models/User');
 const multer = require('multer');
 const jwt = require('jsonwebtoken');
 const path = require('path');
+const fs = require('fs');
 
 const ensureToken = (req, res, next) => {
     const bearerHeader = req.headers['authorization'];
@@ -95,8 +96,19 @@ router.post('/:id', upload.single('avatar'), ensureToken, async (req, res) => {
     const user = await User.findOne({ email: token });
     const url = process.env.URL || 'http://localhost:3003';
 
+    if (user.avatar !== '/assets/images/avatar.png') {
+        fs.unlink(
+            path.join('./uploads/' + user.avatar.split('/')[3]),
+            function async (err) {
+                await user.updateOne({ avatar: '/assets/images/avatar.png'});
+                if (err) throw err;
+
+            }
+        );
+    }
+
     if (user !== null) {
-        await user.update({
+        await user.updateOne({
             avatar: `${url}/${req.file.filename}`,
         });
         return res.status(200).json({ message: 'Uploaded' });
